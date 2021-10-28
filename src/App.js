9a04fbc33f91f5675276a5c024af5e9a21d1494c
nilay <mailnilaydas@gmail.com>
2021-10-28 14:24:27 +0530

    formatted date

--- src/App.js
+++ src/App.js
@@ -8,13 +8,8 @@ function App() {
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
-    {   id: 'e2',
-        title: 'New TV',
-        amount: 799.49,
-        date: new Date(2021, 2, 12)
-    },
-
-      {
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
       id: 'e3',
       title: 'Car Insurance',
       amount: 294.67,
