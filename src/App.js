810b14f710b108c07cb8f1a4d34494e4ae98d9ff
nilay <mailnilaydas@gmail.com>
2021-10-28 17:15:51 +0530

    formatted date

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import Expenses from './components/Expenses';
+import React from 'react';
 
-function App() {
+import Expenses from './components/Expenses/Expenses';
+
+const App = () => {
   const expenses = [
     {
       id: 'e1',
@@ -22,6 +24,8 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
+
+
 
   return (
     <div>
