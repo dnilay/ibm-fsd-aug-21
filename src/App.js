78f48f85b1a9f0438023ab96d6752e9cd87a2b5c
nilay <mailnilaydas@gmail.com>
2021-10-29 12:19:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  handleClick(event){
+    const axios = require('axios');
+    alert(event.target.value)
+
+    axios.get('http://localhost:8082/customers')
+    .then(res=>{
+      console.log(res.data);
+    }).catch(error=>{
+      console.error('Error',error)
+    })
+  }
+
+
+  
+  render() {
+    return (
+      <div className="App">
+       <button onClick={this.handleClick.bind(this)} name="mybutton" value="test">Get Customers</button>
+      </div>
+    );
+  }
 }
 
 export default App;
